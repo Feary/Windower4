4036243faea9f553747c07c861d4c8751b9f7753
Feary <fearyff11@yahoo.com>
2018-04-09 02:14:37 -0400

    Update

--- addons/gearswap/data/User-Globals.lua
+++ addons/gearswap/data/User-Globals.lua
@@ -1,17 +1,1 @@
---Place you can overwrite specific functions and settings with your preferences.
---Extra user functions to change how gearswap functions across jobs.
-
---Time Related Settings (Currently trying to automatically generate offset.)
---Set time_test to true to see and test time settings, change offset to change the settings, requires a capacity ring with uses,
---You want the cap ring offset to be 899 or 900 the MOMENT after using your Capacity ring.
---time_offset = -39601
---time_test = true
-
---If this is set to true it will prevent you from casting shadows when you have more up than that spell would generate.
-conserveshadows = true
-
---Display related settings.
-state.DisplayMode = M(true, 'Display Mode') --Set this to false if you don't want to display modes at the bottom of your screen.
---Uncomment the settings below and change the numbers if you want to move the display to a custom location.
-displayx = 480
-displayy = 750
+--This is where functions go that are for all characters that you play.
