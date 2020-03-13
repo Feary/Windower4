5a305bdb574379f6abb9ad5880b93b740f4da683
Feary <fearyff11@yahoo.com>
2020-03-13 11:38:34 -0400

    Fixed Superwarp For Gil

--- addons/superwarp/map/maps.lua
+++ addons/superwarp/map/maps.lua
@@ -1,8 +1,9 @@
 return {
-	['homepoints'] = require('map/homepoints'),
-	['waypoints'] = require('map/waypoints'),
-	['guides'] = require('map/guides'),
-	['escha'] = require('map/escha'),
-	['unity'] = require('map/unity'),
-	['abyssea'] = require('map/abyssea'),
+    ['homepoints'] = require('map/homepoints'),
+    ['waypoints'] = require('map/waypoints'),
+    ['guides'] = require('map/guides'),
+    ['escha'] = require('map/escha'),
+    ['unity'] = require('map/unity'),
+    ['abyssea'] = require('map/abyssea'),
+    ['protowaypoints'] = require('map/protowaypoints')
 }
